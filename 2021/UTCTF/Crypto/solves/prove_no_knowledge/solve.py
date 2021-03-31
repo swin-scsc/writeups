bb1bf64502040c5474fa8d0fa52f6d768f58390e
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2021-03-31 01:04:25 -0400

    Faster solve

--- 2021/UTCTF/Crypto/solves/prove_no_knowledge/solve.py
+++ 2021/UTCTF/Crypto/solves/prove_no_knowledge/solve.py
@@ -6,20 +6,21 @@ res = tn.read_until(b'mod p.\n')
 p = int(str(res).split('p: ')[1].split('y:')[0][:-2])
 y = int(str(res).split('y: ')[1][:-37])
 
-x1 = 0
-x2 = 1
-x3 = pow(y, -1, p)
-x4 = 0
+x1 = b'0'
+x2 = b'1'
+x3 = str(pow(y, -1, p)).encode()
+x4 = b'0'
+
+payload = b''
 
 for i in range(256):
     if i % 2 == 0:
-        tn.write((str(x2) + '\n').encode())
+        payload += x2 + b'\n' + x1 + b'\n'
     else:
-        tn.write((str(x3) + '\n').encode())
-    print(tn.read_until(b'.\n'))
+        payload += x3 + b'\n' + x4 + b'\n'
     
-    if i % 2 == 0:
-        tn.write((str(x1) + '\n').encode())
-    else:
-        tn.write((str(x4) + '\n').encode())
-    print(tn.read_until(b'.\n'))
+tn.write(payload)
+
+for i in range(511): tn.read_until(b'.\n')
+
+print(tn.read_until(b'.\n'))
